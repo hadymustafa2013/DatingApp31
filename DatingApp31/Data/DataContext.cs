89bada40586cf61138a8af450c0d8ea9f17eb8b5
Hady Mustafa <hmustafa@riskmatrixirms.com>
2020-06-21 19:30:50 +0300

    .

---
+++ DatingApp31/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp31.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp31.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
--- DatingApp31/Startup.cs
+++ DatingApp31/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp31.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp31
 {
@@ -24,6 +26,7 @@ namespace DatingApp31
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration["Data:DatingApp31:ConnectionString"]));
             services.AddControllers();
         }
 
