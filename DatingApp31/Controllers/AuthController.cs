ec96b14457c7d732cac1e76244c77d71b11ca9c7
Hady Mustafa <hmustafa@riskmatrixirms.com>
2020-06-23 18:19:32 +0300

    .

---
+++ DatingApp31/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp31.Data;
+using DatingApp31.Dtos;
+using DatingApp31.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp31.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegDto)
+        {
+            // Validate request
+
+            if (await _repo.UserExists(userForRegDto.UserName.ToLower()))
+                return BadRequest("User already exists!");
+
+            var userToCreate = new User()
+            {
+                UserName = userForRegDto.UserName.ToLower()
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegDto.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async void Login()
+        {
+        }
+    }
+}
--- DatingApp31/Startup.cs
+++ DatingApp31/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp31
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration["Data:DatingApp31:ConnectionString"]));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
